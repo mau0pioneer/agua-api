98f587f875be7950486ac4e1fe0f2949c16b0ce5
Mauricio Martinez <mauriciomartinez@MacBook-Air-de-Mauricio.local>
2024-04-28 21:36:18 -0600

    add dockerfile

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hello World";
+    return "Holamundo";
 });
 
 Route::get('/report', [SystemController::class, 'report']);
